b4496818a1d8c3db4dad58bf617cfe6048172660
KrapBoss <ttey0902@gmail.com>
2024-08-02 00:52:54 +0900

    24-8-2

--- ConsoleApp1/ConsoleApp1/E1655__가운데를말해요.cs
+++ ConsoleApp1/ConsoleApp1/E1655__가운데를말해요.cs
@@ -1,58 +1,52 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Text;
 
-namespace ConsoleApp1
-{
-    internal class E1655__가운데를말해요
-    {
-        static void Main(string[] args)
-        {
-            int N(StreamReader r) => int.Parse(r.ReadLine());
+//namespace ConsoleApp1
+//{
+//    internal class E1655__가운데를말해요
+//    {
+//        static void Main(string[] args)
+//        {
+//            int N(StreamReader r) => int.Parse(r.ReadLine());
             
-            using (StreamReader reader = new StreamReader(Console.OpenStandardInput()))
-            {
-                int iter = N(reader);
-                List<int> list = new List<int>();
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < iter; i++)
-                {
-                    int n = N(reader);
+//            using (StreamReader reader = new StreamReader(Console.OpenStandardInput()))
+//            {
+//                int iter = N(reader);
+//                List<int> list = new List<int>();
+//                StringBuilder sb = new StringBuilder();
+//                for (int i = 0; i < iter; i++)
+//                {
+//                    int n = N(reader);
 
-                    int left = 0;
-                    int right = list.Count;
+//                    int left = 0;
+//                    int right = list.Count;
 
-                    while(left < right)
-                    {
-                        int mid = (left + right) / 2;
+//                    while(left < right)
+//                    {
+//                        int mid = (left + right) / 2;
 
-                        if (n>list[mid])
-                        {
-                            left =mid+1;
-                        }
-                        else
-                        {
-                            right = mid;
-                        }
-                    }
+//                        if (n>list[mid])
+//                        {
+//                            left =mid+1;
+//                        }
+//                        else
+//                        {
+//                            right = mid;
+//                        }
+//                    }
 
-                    list.Insert(left, n);
+//                    list.Insert(left, n);
 
-                    sb.Append(list[(list.Count - 1) / 2]);
-                    sb.AppendLine();
-                }
+//                    sb.Append(list[(list.Count - 1) / 2]);
+//                    sb.AppendLine();
+//                }
 
-                using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput()))
-                    sw.WriteLine(sb);
-            }
-
-            using StreamWriter w = new(Console.OpenStandardOutput());
-            var f = () => int.Parse(Console.ReadLine()); 
-            int n = f(), k, i, s; 
-            List<int> l = new();
-            while (n-- > 0) { k = f(); i = l.BinarySearch(k); l.Insert(i < 0 ? ~i : i, k); s = l.Count; w.WriteLine(l[s / 2 - (1 - s % 2)]); }
-        }
-    }
-}
+//                using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput()))
+//                    sw.WriteLine(sb);
+//            }
+//        }
+//    }
+//}
--- ConsoleApp1/ConsoleApp1/Test.cs
+++ ConsoleApp1/ConsoleApp1/Test.cs
@@ -1,38 +1,19 @@
 ﻿//using System;
+//using System.Collections;
 //using System.Collections.Generic;
 //using System.Linq;
 
 //namespace ConsoleApp1
 //{
-//    class Pet
-//    {
-//        public string Name { get; set; }
-//        public int Age { get; set; }
-//    }
-
 //    internal class Test // 웰컴 키트 
 //    {
 //        static void Main(string[] args)
 //        {
-//            string[] fruits = { "grape", "passionfruit", "banana", "mango",
-//                      "orange", "raspberry", "apple", "blueberry" };
-
-//            //이미 정렬된 시퀀스를 다시 문자열의 사전 순으로 정렬합니다.
-//            IEnumerable<string> query =
-//                fruits.OrderBy(fruit => fruit.Length).ThenByDescending(fruit => fruit);
-
-//            foreach (string fruit in query)
-//            {
-//                Console.WriteLine(fruit);
-//            }
-
+//            int o1 = 4;
+//            Console.WriteLine(Convert.ToString(o1, 2));
+//            int o2 = -4;
+//            Console.WriteLine(Convert.ToString(o2,2));
+//            Console.WriteLine(Convert.ToString(o1+o2, 2));
 //        }
 //    }
-
-//    // This code produces the following output:
-//    //
-//    // {Owner=Higa, Pet=Scruffy}
-//    // {Owner=Higa, Pet=Sam}
-//    // {Owner=Ashkenazi, Pet=Sugar}
-//    // {Owner=Price, Pet=Scratches}
 //}
