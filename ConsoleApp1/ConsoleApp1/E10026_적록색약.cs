6f421f7bb997ce60a2acfcfc860cc1b8076f55fe
KrapBoss <ttey0902@gmail.com>
2024-08-04 18:51:22 +0900

    24-8-4

--- ConsoleApp1/ConsoleApp1/E10026_적록색약.cs
+++ ConsoleApp1/ConsoleApp1/E10026_적록색약.cs
@@ -1,81 +1,81 @@
-﻿using System;
-using System.Linq;
+﻿//using System;
+//using System.Linq;
 
-namespace ConsoleApp1
-{
-    internal class E10026_적록색약
-    {
-        static void Main(string[] args)
-        {
-            //4방향 정의
-            int[] dx = new int[] { -1, 1, 0, 0 };
-            int[] dy = new int[] { 0, 0, -1, 1 };
+//namespace ConsoleApp1
+//{
+//    internal class E10026_적록색약
+//    {
+//        static void Main(string[] args)
+//        {
+//            //4방향 정의
+//            int[] dx = new int[] { -1, 1, 0, 0 };
+//            int[] dy = new int[] { 0, 0, -1, 1 };
 
-            //같은 색상 탐색을 위한 재귀함수
-            void BPS(char[][] a, bool[,] c, int x, int y)
-            {
-                if (c[x, y]) return;
+//            //같은 색상 탐색을 위한 재귀함수
+//            void BPS(char[][] a, bool[,] c, int x, int y)
+//            {
+//                if (c[x, y]) return;
 
-                c[x, y] = true;
+//                c[x, y] = true;
 
-                int len = a[0].Length;
-                for(int i = 0; i < 4; i++)
-                {
-                    int X = x + dx[i];
-                    int Y = y + dy[i];
+//                int len = a[0].Length;
+//                for(int i = 0; i < 4; i++)
+//                {
+//                    int X = x + dx[i];
+//                    int Y = y + dy[i];
 
-                    if (X < 0 || Y < 0 || X >= len || Y >= len) continue;
+//                    if (X < 0 || Y < 0 || X >= len || Y >= len) continue;
 
-                    if (a[x][y] == a[X][Y])
-                    {
-                        BPS(a, c, X, Y);
-                    }
-                }
-            }
+//                    if (a[x][y] == a[X][Y])
+//                    {
+//                        BPS(a, c, X, Y);
+//                    }
+//                }
+//            }
 
-            string input() =>Console.ReadLine();
+//            string input() =>Console.ReadLine();
 
-            int n = int.Parse(input());
+//            int n = int.Parse(input());
 
-            char[][] arr = new char[n][];
-            bool[,] check = new bool[n,n];
+//            char[][] arr = new char[n][];
+//            bool[,] check = new bool[n,n];
 
-            //D => 적록 | S => 정상 시야
-            int D = 0, S = 0;
+//            //D => 적록 | S => 정상 시야
+//            int D = 0, S = 0;
 
-            for(int i =0;i < n; i++)
-            {
-                arr[i] = input().Select(x=> x).ToArray();
-            }
+//            for(int i =0;i < n; i++)
+//            {
+//                arr[i] = input().Select(x=> x).ToArray();
+//            }
 
-            for(int i = 0;i < n;i++)
-            {
-                for(int j = 0;j < n; j++)
-                {
-                    if (!check[i, j])
-                    {
-                        S++;
-                        BPS(arr,check,i,j);
-                    }
-                }
-            }
+//            for(int i = 0;i < n;i++)
+//            {
+//                for(int j = 0;j < n; j++)
+//                {
+//                    if (!check[i, j])
+//                    {
+//                        S++;
+//                        BPS(arr,check,i,j);
+//                    }
+//                }
+//            }
 
-            //G 를 R 로 변환 후 재탐색
-            check = new bool[n, n];
-            arr = arr.Select(x=> x.Select(y => y.Equals('G') ? 'R' : y).ToArray()).ToArray();
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if (!check[i, j])
-                    {
-                        D++;
-                        BPS(arr, check, i, j);
-                    }
-                }
-            }
+//            //G 를 R 로 변환 후 재탐색
+//            check = new bool[n, n];
+//            arr = arr.Select(x=> x.Select(y => y.Equals('G') ? 'R' : y).ToArray()).ToArray();
+//            for (int i = 0; i < n; i++)
+//            {
+//                for (int j = 0; j < n; j++)
+//                {
+//                    if (!check[i, j])
+//                    {
+//                        D++;
+//                        BPS(arr, check, i, j);
+//                    }
+//                }
+//            }
 
-            Console.WriteLine($"{S} {D}");
-        }
-    }
-}
+//            Console.WriteLine($"{S} {D}");
+//        }
+//    }
+//}
---
+++ ConsoleApp1/ConsoleApp1/E16928_뱀과사다리게임.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class E16928_뱀과사다리게임
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] input() => Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+
+            int[] location = new int[101];
+
+            int[] iter = input();
+            for(int i=0;i<iter[0] + iter[1]; i++)
+            {
+                int[] _tmp = input();
+                location[_tmp[0]] = _tmp[1];
+            }
+
+            int[] save = new int[101];
+
+            Queue<Tuple<int,int>> spot = new Queue<Tuple<int, int>>();
+            spot.Enqueue(new Tuple<int, int> (1,-1));
+            while (spot.Count > 0)
+            {
+                var s = spot.Dequeue();
+
+                if (save[s.Item1] <= s.Item2 && (save[s.Item1] != 0)) continue;
+
+                save[s.Item1] = s.Item2 +1;
+
+                int floor = s.Item1;
+                if (location[s.Item1] != 0) floor = location[s.Item1];
+
+                for (int i = 0; i < 6; i++)
+                {
+                    int next = floor + 1 + i;
+                    if (next > 100) break;
+
+                    spot.Enqueue(new Tuple<int, int>(next , save[s.Item1]));
+                }
+            }
+
+            Console.WriteLine(save[100]);
+        }
+    }
+}
