3beac675bcb901a0102aeb733f2697bc07d64fb0
KrapBoss <ttey0902@gmail.com>
2024-08-04 19:51:53 +0900

    24-8-4

--- ConsoleApp1/ConsoleApp1/E16928_뱀과사다리게임.cs
+++ ConsoleApp1/ConsoleApp1/E16928_뱀과사다리게임.cs
@@ -1,51 +1,60 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace ConsoleApp1
-{
-    internal class E16928_뱀과사다리게임
-    {
+//namespace ConsoleApp1
+//{
+//    internal class E16928_뱀과사다리게임
+//    {
         
-        static void Main(string[] args)
-        {
-            int[] input() => Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+//        static void Main(string[] args)
+//        {
+//            int[] input() => Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
 
-            int[] location = new int[101];
+//            //맵의 사다리와 뱀의 값을 저장하기 위함
+//            int[] location = new int[101];
+            
+//            //입력값을 받아옵니다.
+//            int[] iter = input();
+//            for(int i=0;i<iter[0] + iter[1]; i++)
+//            {
+//                int[] _tmp = input();
+//                location[_tmp[0]] = _tmp[1];
+//            }
 
-            int[] iter = input();
-            for(int i=0;i<iter[0] + iter[1]; i++)
-            {
-                int[] _tmp = input();
-                location[_tmp[0]] = _tmp[1];
-            }
+//            //현재 탐색된 주사위 값의 저장값
+//            int[] save = new int[101];
 
-            int[] save = new int[101];
+//            //<현재 위치, 주사위 횟수> 를 저장함으로서, 현재의 위치값에 따른 주사위 횟수를 저장한다.
+//            Queue<Tuple<int,int>> spot = new Queue<Tuple<int, int>>();
+//            spot.Enqueue(new Tuple<int, int> (1,-1));
+//            while (spot.Count > 0)
+//            {
+//                var s = spot.Dequeue();
 
-            Queue<Tuple<int,int>> spot = new Queue<Tuple<int, int>>();
-            spot.Enqueue(new Tuple<int, int> (1,-1));
-            while (spot.Count > 0)
-            {
-                var s = spot.Dequeue();
+//                //현재 주사위 값이 저장된 배열과의 비교와 0이 아닌 값을 판단한다.
+//                if (save[s.Item1] <= s.Item2 && (save[s.Item1] != 0)) continue;
 
-                if (save[s.Item1] <= s.Item2 && (save[s.Item1] != 0)) continue;
+//                //현재 위치에 주사위 값을 지정한다.
+//                save[s.Item1] = s.Item2 +1;
 
-                save[s.Item1] = s.Item2 +1;
+//                //맵에서 현재의 사다리 또는 뱀일 경우 자리를 이동시킵니다.
+//                int floor = s.Item1;
+//                if (location[s.Item1] != 0) floor = location[s.Item1];
 
-                int floor = s.Item1;
-                if (location[s.Item1] != 0) floor = location[s.Item1];
+//                //주사위 6눈의 값만큼 이동시킵니다.
+//                for (int i = 0; i < 6; i++)
+//                {
+//                    int next = floor + 1 + i;
+//                    if (next > 100) break;
 
-                for (int i = 0; i < 6; i++)
-                {
-                    int next = floor + 1 + i;
-                    if (next > 100) break;
+//                    spot.Enqueue(new Tuple<int, int>(next , save[s.Item1]));
+//                }
+//            }
 
-                    spot.Enqueue(new Tuple<int, int>(next , save[s.Item1]));
-                }
-            }
-
-            Console.WriteLine(save[100]);
-        }
-    }
-}
+//            //마지막 위치에 도달한 값을 출력한다
+//            Console.WriteLine(save[100]);
+//        }
+//    }
+//}
---
+++ ConsoleApp1/ConsoleApp1/E7662_이중우선순위큐연산.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class E7662_이중우선순위큐연산
+    {
+        static void Main(string[] args)
+        {
+            string[] input(StreamReader s)=> s.ReadLine().Split();
+            
+            using(StreamReader st = new StreamReader(Console.OpenStandardInput())) {
+                int a = int.Parse(input(st)[0]);
+
+                List<int> list = new List<int>();
+                for (int i = 0; i < a; i++)
+                {
+                    int b = int.Parse(input(st)[0]);
+                    for (int j = 0; j < b; j++)
+                    {
+                        string[] inp = input(st);
+                        int number = int.Parse(inp[1]);
+                        if (inp[0].Equals("I"))
+                        {
+                            int index = list.BinarySearch(number);
+                            if (index < 0) list.Insert(~index, number);
+                            else list.Insert(index, number);
+                        }
+                        else
+                        {
+                            if (list.Count == 0) continue;
+                            if (number < 0) list.RemoveAt(0);
+                            else list.RemoveAt(list.Count - 1);
+                        }
+                    }
+                }
+            
+                using(StreamWriter sw = new StreamWriter(Console.OpenStandardOutput()))
+                {
+                    if (list.Count > 0)
+                    {
+                        sw.WriteLine($"{list[list.Count - 1]} {list[0]}");
+                    }
+                    else
+                    {
+                        sw.WriteLine("Empty");
+                    }
+                }
+            }
+        }
+    }
+}
