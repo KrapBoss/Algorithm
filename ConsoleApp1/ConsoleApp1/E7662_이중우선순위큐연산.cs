cf5557b0a2bf0dea508ab563c73a6339a526ded4
KrapBoss <ttey0902@gmail.com>
2024-08-08 01:42:32 +0900

    7662 안됨

--- ConsoleApp1/ConsoleApp1/E7662_이중우선순위큐연산.cs
+++ ConsoleApp1/ConsoleApp1/E7662_이중우선순위큐연산.cs
@@ -1,19 +1,64 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Text;
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Text;
+using System.Linq;
 
-//namespace ConsoleApp1
-//{
-//    internal class E7662_이중우선순위큐연산
-//    {
-//        static void Main(string[] args)
-//        {
-//            string[] input(StreamReader s) => s.ReadLine().Split();
+namespace consoleapp1
+{
+    internal class E7662_이중우선순위큐연산
+    {
+        static void Main(string[] args)
+        {
+            string[] input(StreamReader s) => s.ReadLine().Split();
 
-//            Queue<int> q = new Queue<int>();
-           
-//        }
-//    }
-//}
+            using (StreamReader st = new StreamReader(Console.OpenStandardInput()))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                int a = int.Parse(input(st)[0]);
+
+                SortedList<int, int> SL = new SortedList<int, int>();
+
+                //테스트 케이스
+                for (int i = 0; i < a; i++)
+                {
+                    // 명령 입력 횟수만큼 반복
+                    int b = int.Parse(input(st)[0]);
+
+                    for (int j = 0; j < b; j++)
+                    {
+                        string[] inp = input(st);
+                        int number = int.Parse(inp[1]);
+                        if (inp[0].Equals("I"))
+                        {
+                            if (SL.ContainsKey(number)) SL[number]++;
+                            else SL.Add(number, 1);
+
+                        }
+                        else
+                        {
+                            if (SL.Count < 1) continue;
+
+                            int key = (number < 0) ? SL.Keys[0] : SL.Keys[SL.Count - 1];
+
+                            --SL[key];
+                            if (SL[key] < 1) SL.Remove(key);
+                        }
+                    }
+
+                    if (SL.Count < 1)
+                    {
+                        sb.AppendLine("Empty");
+                    }
+                    else
+                    {
+                        sb.AppendLine($"{SL.Keys[SL.Count - 1]} {SL.Keys[0]}");
+                    }
+                }
+
+                Console.WriteLine(sb);
+            }
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/E9019_DSLR.cs
+++ ConsoleApp1/ConsoleApp1/E9019_DSLR.cs
@@ -1,72 +1,72 @@
-﻿using System.Collections.Generic;
-using System;
-using System.Linq;
-using System.IO;
-using System.Text;
+﻿//using System.Collections.Generic;
+//using System;
+//using System.Linq;
+//using System.IO;
+//using System.Text;
 
-namespace ConsoleApp1
-{
-    struct info{
-        public int num;
-        public string mandate;
-    }
-    internal class TEST
-    {
-        static void Main(string[] args)
-        {
-            StringBuilder sb = new StringBuilder();
-            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
-            {
-                int iter = int.Parse(sr.ReadLine());
-                for (int i = 0; i < iter; i++)
-                {
-                    int[] v = sr.ReadLine().Split().Select(int.Parse).ToArray();
+//namespace ConsoleApp1
+//{
+//    struct info{
+//        public int num;
+//        public string mandate;
+//    }
+//    internal class TEST
+//    {
+//        static void Main(string[] args)
+//        {
+//            StringBuilder sb = new StringBuilder();
+//            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+//            {
+//                int iter = int.Parse(sr.ReadLine());
+//                for (int i = 0; i < iter; i++)
+//                {
+//                    int[] v = sr.ReadLine().Split().Select(int.Parse).ToArray();
 
-                    Queue<info> q = new Queue<info>();
-                    q.Enqueue(new info { num = v[0], mandate = "" });
-                    bool[] register = new bool[10000];
+//                    Queue<info> q = new Queue<info>();
+//                    q.Enqueue(new info { num = v[0], mandate = "" });
+//                    bool[] register = new bool[10000];
 
-                    while (true)
-                    {
-                        var a = q.Dequeue();
-                        if (a.num == v[1])
-                        {
-                            sb.AppendLine(a.mandate);
-                            break;
-                        }
+//                    while (true)
+//                    {
+//                        var a = q.Dequeue();
+//                        if (a.num == v[1])
+//                        {
+//                            sb.AppendLine(a.mandate);
+//                            break;
+//                        }
 
-                        //D
-                        int D = a.num * 2 % 10000;
-                        if (!register[D])
-                        {
-                            register[D] = true;
-                            q.Enqueue(new info { num = D, mandate = $"{a.mandate}D" });
-                        }
-                        //S
-                        int S = (a.num - 1 + 10000) % 10000;
-                        if (!register[S])
-                        {
-                            register[S] = true;
-                            q.Enqueue(new info { num = S, mandate = $"{a.mandate}S" });
-                        }
-                        //L
-                        int L = a.num % 1000 * 10 + a.num / 1000;
-                        if (!register[L])
-                        {
-                            register[L] = true;
-                            q.Enqueue(new info { num = L, mandate = $"{a.mandate}L" });
-                        }
-                        //R
-                        int R = a.num / 10 + a.num % 10 * 1000;
-                        if (!register[R])
-                        {
-                            register[R] = true;
-                            q.Enqueue(new info { num = R, mandate = $"{a.mandate}R" });
-                        }
-                    }
-                }
-            }
-            Console.WriteLine(sb);
-        }
-    }
-}
+//                        //D
+//                        int D = a.num * 2 % 10000;
+//                        if (!register[D])
+//                        {
+//                            register[D] = true;
+//                            q.Enqueue(new info { num = D, mandate = $"{a.mandate}D" });
+//                        }
+//                        //S
+//                        int S = (a.num - 1 + 10000) % 10000;
+//                        if (!register[S])
+//                        {
+//                            register[S] = true;
+//                            q.Enqueue(new info { num = S, mandate = $"{a.mandate}S" });
+//                        }
+//                        //L
+//                        int L = a.num % 1000 * 10 + a.num / 1000;
+//                        if (!register[L])
+//                        {
+//                            register[L] = true;
+//                            q.Enqueue(new info { num = L, mandate = $"{a.mandate}L" });
+//                        }
+//                        //R
+//                        int R = a.num / 10 + a.num % 10 * 1000;
+//                        if (!register[R])
+//                        {
+//                            register[R] = true;
+//                            q.Enqueue(new info { num = R, mandate = $"{a.mandate}R" });
+//                        }
+//                    }
+//                }
+//            }
+//            Console.WriteLine(sb);
+//        }
+//    }
+//}
